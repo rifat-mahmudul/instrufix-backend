8424c8b8311d53c2bf2ba6dd072599d525318661
jarifMohammed <mohammedjarif@protonmail.com>
2026-06-08 22:12:09 +0600

    issue fixed

--- src/modules/instrumentFamily/instrumentFamily.router.js
+++ src/modules/instrumentFamily/instrumentFamily.router.js
@@ -1,15 +1,15 @@
-const { Router } = require("express");
-const instrumentController = require("./instrumentFamily.controller");
+const { Router } = require('express');
+const instrumentController = require('./instrumentFamily.controller');
 
 const router = Router();
 
 //only admin can create
-router.post("/create", instrumentController.createInstrument);
-router.get("/", instrumentController.getAllInstrument);
-router.get("/:id", instrumentController.getInstrumentById);
-router.put("/:instrumentId", instrumentController.updateInstrumentName);
-router.put("/:instrumentId/type/:typeId", instrumentController.updateInstrument);
-router.delete("/:instrumentId", instrumentController.deleteInstrument);
+router.post('/create', instrumentController.createInstrument);
+router.get('/', instrumentController.getAllInstrument);
+router.get('/:id', instrumentController.getInstrumentById);
+router.put('/:instrumentId', instrumentController.updateInstrumentName);
+router.put('/:instrumentId/type/:typeId', instrumentController.updateInstrument);
+router.delete('/:instrumentId', instrumentController.deleteInstrument);
 
 const instrumentFamilyRouter = router;
 module.exports = instrumentFamilyRouter;
--- src/modules/instrumentFamily/instrumentFamily.service.js
+++ src/modules/instrumentFamily/instrumentFamily.service.js
@@ -1,4 +1,4 @@
-const InstrumentFamilyModel = require("./instrumentFamily.model");
+const InstrumentFamilyModel = require('./instrumentFamily.model');
 
 const createInstrument = async (payload) => {
   const result = await InstrumentFamilyModel.create(payload);
@@ -6,41 +6,39 @@ const createInstrument = async (payload) => {
 };
 
 const getAllInstrument = async ({ id, name }) => {
-
   if (id) {
     // Find the parent document that contains this nested type
     const instrumentFamily = await InstrumentFamilyModel.findOne({
-      "instrumentTypes._id": id
-    }).lean().exec();
+      'instrumentTypes._id': id,
+    })
+      .lean()
+      .exec();
 
     if (!instrumentFamily) return null;
 
     // Convert _id to string for comparison
-    const typeObj = instrumentFamily.instrumentTypes.find(
-      t => t._id.toString() === id
-    );
+    const typeObj = instrumentFamily.instrumentTypes.find((t) => t._id.toString() === id);
 
-    return typeObj; 
+    return typeObj;
   }
-  
+
   if (name) {
     const instrumentFamily = await InstrumentFamilyModel.findOne({
-      "instrumentTypes.type": name
-    }).lean().exec();
+      'instrumentTypes.type': name,
+    })
+      .lean()
+      .exec();
 
     if (!instrumentFamily) return null;
 
-    const typeObj = instrumentFamily.instrumentTypes.find(
-      t => t.type === name
-    );
+    const typeObj = instrumentFamily.instrumentTypes.find((t) => t.type === name);
 
-    return typeObj; 
+    return typeObj;
   }
 
   const allInstruments = await InstrumentFamilyModel.find({}).lean().exec();
   return allInstruments;
 };
-
 
 const getInstrumentById = async (id) => {
   return await InstrumentFamilyModel.findById(id);
@@ -49,66 +47,123 @@ const getInstrumentById = async (id) => {
 const updateInstrumentName = async (instrumentId, payload) => {
   const instrument = await InstrumentFamilyModel.findById(instrumentId);
   if (!instrument) {
-    throw new Error("Instrument not found");
+    throw new Error('Instrument not found');
   }
 
-  const result = await InstrumentFamilyModel.findByIdAndUpdate(
-    instrumentId,
-    payload,
-    { new: true }
-  );
+  const result = await InstrumentFamilyModel.findByIdAndUpdate(instrumentId, payload, {
+    new: true,
+  });
   return result;
 };
 
-
-
 // Update Instrument Service
-const updateInstrument = async (instrumentId, typeId, payload) => {
+// const updateInstrument = async (instrumentId, typeId, payload) => {
 
-  const instrument = await InstrumentFamilyModel.findById(instrumentId);
+//   const instrument = await InstrumentFamilyModel.findById(instrumentId);
+//   if (!instrument) {
+//     throw new Error("Instrument family not found");
+//   }
+
+//   let updateQuery = {};
+
+//   if (payload.replace) {
+
+//     updateQuery = {
+//       $set: {
+//         "instrumentTypes.$.serviceType": payload.serviceType,
+//       },
+//     };
+//   } else {
+
+//     updateQuery = {
+//       $addToSet: {
+//         "instrumentTypes.$.serviceType": { $each: payload.serviceType },
+//       },
+//     };
+//   }
+
+//   const result = await InstrumentFamilyModel.findOneAndUpdate(
+//     { _id: instrumentId, "instrumentTypes._id": typeId },
+//     updateQuery,
+//     { new: true }
+//   );
+
+//   if (!result) {
+//     throw new Error("Instrument type not found");
+//   }
+
+//   return result;
+// };
+
+const updateInstrument = async (instrumentId, typeId, payload) => {
+  const { action, serviceType } = payload;
+
+  if (!Array.isArray(serviceType)) {
+    throw new Error('serviceType must be an array');
+  }
+
+  const instrument = await InstrumentFamilyModel.findOne({
+    _id: instrumentId,
+    'instrumentTypes._id': typeId,
+  });
+
   if (!instrument) {
-    throw new Error("Instrument family not found");
+    throw new Error('Instrument family or instrument type not found');
   }
 
   let updateQuery = {};
 
-  if (payload.replace) {
-   
-    updateQuery = {
-      $set: {
-        "instrumentTypes.$.serviceType": payload.serviceType,
-      },
-    };
-  } else {
-    
-    updateQuery = {
-      $addToSet: {
-        "instrumentTypes.$.serviceType": { $each: payload.serviceType },
-      },
-    };
+  switch (action) {
+    case 'add':
+      updateQuery = {
+        $addToSet: {
+          'instrumentTypes.$.serviceType': {
+            $each: serviceType,
+          },
+        },
+      };
+      break;
+
+    case 'remove':
+      updateQuery = {
+        $pull: {
+          'instrumentTypes.$.serviceType': {
+            $in: serviceType,
+          },
+        },
+      };
+      break;
+
+    case 'replace':
+      updateQuery = {
+        $set: {
+          'instrumentTypes.$.serviceType': serviceType,
+        },
+      };
+      break;
+
+    default:
+      throw new Error('Invalid action. Use add, remove, or replace');
   }
 
   const result = await InstrumentFamilyModel.findOneAndUpdate(
-    { _id: instrumentId, "instrumentTypes._id": typeId },
+    {
+      _id: instrumentId,
+      'instrumentTypes._id': typeId,
+    },
     updateQuery,
-    { new: true }
+    {
+      new: true,
+    },
   );
-
-  if (!result) {
-    throw new Error("Instrument type not found");
-  }
 
   return result;
 };
 
-
-
-
-
 const deleteInstrument = async (instrumentId) => {
   const instrument = await InstrumentFamilyModel.findById(instrumentId);
   if (!instrument) {
-    throw new Error("Instrument not found");
+    throw new Error('Instrument not found');
   }
 
   const result = await InstrumentFamilyModel.findByIdAndDelete(instrumentId);
